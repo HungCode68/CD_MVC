97dd4ad6297804a24451d9f6fb75a995e67d6e38
HungCode68 <nguyenhungct6868@gmail.com>
2025-06-13 08:20:31 +0700

    v2

--- obj/Debug/net8.0/NailMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/NailMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NailMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9058bd784f5a5391f806614a0ec844e3d1111ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("NailMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NailMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
